c6b75d097d1fea13375f007b1170157191e4135f
CleydsonLioncio <cleydson.vocal@gmail.com>
2023-02-22 20:20:30 -0300

    array 2

---
+++ array2.js
@@ -0,0 +1,5 @@
+const numbers = new Array(10);
+console.log(numbers)
+
+// tem que ter cuidado ao passar apen as um número no
+// módulo do array pois pode ser interpretado como a posição do elemento
