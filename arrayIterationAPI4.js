b08966bca9da0058cd6b73d499ccb71da7c2dd4c
CleydsonLioncio <cleydson.vocal@gmail.com>
2023-02-23 20:13:52 -0300

    feat: array iteration API 4

---
+++ arrayIterationAPI4.js
@@ -0,0 +1,18 @@
+const frameworks = [
+  {
+    name: "Angular.js",
+    contributors: 1548
+  },
+  {
+    name: "Ember.js",
+    contributors: 746
+  },
+  {
+    name: "Vue.js",
+    contributors: 240
+  }
+];
+const result = frameworks.some(function (framework) {
+  return framework.name.includes("js");
+});
+console.log(result)
