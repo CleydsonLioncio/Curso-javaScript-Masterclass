911f74a5a44e365cbb6184edddc9862ef19c492c
CleydsonLioncio <cleydson.vocal@gmail.com>
2023-02-27 18:15:15 -0300

    feat: array accessor API 2

---
+++ arrayAccessorAPI2.js
@@ -0,0 +1,4 @@
+const oolanguages = ["Smalltalk", "C++", "Simula"];
+const funcionalLanguages = ["Haskell", "Scheme"];
+const languages = [].concat(oolanguages, funcionalLanguages);
+console.log(languages)
